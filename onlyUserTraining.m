9c76325e12360d799c87e0fd887dd1b5105468ca
kabirivan <aguas.xavier2013a@gmail.com>
2020-05-28 23:03:32 -0500

    Add read data

---
+++ onlyUserTraining.m
@@ -0,0 +1,17 @@
+close all
+clear all
+clc
+
+
+%% Load training data  
+
+folders = dir('training');
+numFolders = length(folders);
+numUsertraining = numFolders;
+
+
+for i = 1:numFolders
+    
+   
+    
+end
